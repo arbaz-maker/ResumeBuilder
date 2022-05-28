43c9813b96c4eb6df74109849b91f45603c0157b
[arbaz.sohail@outlook.com] <63145796+arbaz-maker@users.noreply.github.com>
2022-05-28 21:31:26 +0500

    Added New Frontend project

--- ResumeBuilderBackend/app.js
+++ ResumeBuilderBackend/app.js
@@ -9,6 +9,6 @@ app.use(express.json());
 
 app.use('/', routes); //to use the routes
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 8080, () => {
     console.log('Your app is listening on port ' + listener.address().port)
 })
